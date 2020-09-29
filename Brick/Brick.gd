5bc340264382c82d5abd2df89f89a7c561aee450
cnfarmer99 <54722585+cnfarmer99@users.noreply.github.com>
2020-09-29 18:03:18 -0400

    bricks are correct colors

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -10,9 +10,29 @@ func _ready():
 
 func update_color():
 	if HUD.color_blocks:
-		pass
+		if row == 0:
+			# red 8
+			$Color.color = Color8(224, 49, 49)
+		if row == 1:
+			# orange 6
+			$Color.color = Color8(253, 126, 20)
+		if row == 2:
+			# yellow 3
+			$Color.color = Color8(255, 224, 102)
+		if row == 3:
+			# lime 5
+			$Color.color = Color8(148, 216, 45)
+		if row == 4:
+			# blue 6
+			$Color.color = Color8(34, 139, 230)
+		if row == 5:
+			# violet 5
+			$Color.color = Color8(132, 94, 247)
+		if row == 6:
+			# grape 6
+			$Color.color = Color8(190, 75, 219)
 	else:
-		pass
+		$Color.color = Color(1, 1, 1, 1)
 
 func emit_particle(pos):
 	if HUD.particle_blocks:
